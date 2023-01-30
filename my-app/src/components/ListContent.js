e1c8a5ce45f90e16b564b8125b0d1ee7ad36d45e
deviant <95397536+Vedanivas@users.noreply.github.com>
2023-01-30 11:21:08 +0530

    Add files via upload

---
+++ my-app/src/components/ListContent.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {useHistory} from "react-router-dom"
+
+export default function ListContent () {
+
+    return (
+        <ListItem alignItems="flex-start"
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                    }>
+        <ListItemAvatar>
+          <Avatar src="/broken-image.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    )
+}
